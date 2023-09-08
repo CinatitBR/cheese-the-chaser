5e1b155c7cabcb16ef626d6c7329e68aea044044
Igor Rocha <cinatit.igor.santos@gmail.com>
2023-09-08 16:02:20 -0300

    Create chase()

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let idAnimationFrame;
+const SPEED = 1; // Movement speed
 
 const cheese = document.getElementById("cheese");
 let isPressed = false;
 
 const rat = document.getElementById("rat");
+const ant = document.querySelector(".ant");
 
 // Get positions
 const ratPos = {
@@ -14,6 +16,14 @@ const cheesePos = {
     left: cheese.getBoundingClientRect().left,
     top: cheese.getBoundingClientRect().top
 };
+const antPos = {
+    left: ant.getBoundingClientRect().left,
+    top: ant.getBoundingClientRect().top
+};
+
+rat.position = ratPos;
+ant.position = antPos;
+cheese.position = cheesePos;
 
 cheese.addEventListener("mousedown", (ev) => {
     isPressed = true;
@@ -47,23 +57,50 @@ function updatePositions() {
 
     axis: left or top, the axis to move.
 */
-function moveRat(rat, axis) {
-    // Movement speed
-    const speed = 1.5;
-    
+function moveRat(rat, axis) {    
     // Strategy to know if I should sum or subtract the speed.
     let sign = (cheesePos[axis] - ratPos[axis]);
     sign = sign / Math.abs(sign);
 
     // Round position
     if (sign > 0) {
-        ratPos[axis] = Math.min(ratPos[axis] + (sign * speed), cheesePos[axis]);
+        ratPos[axis] = Math.min(ratPos[axis] + (sign * SPEED), cheesePos[axis]);
     }
     else if (sign < 0) {
-        ratPos[axis] = Math.max(ratPos[axis] + (sign * speed), cheesePos[axis]);
+        ratPos[axis] = Math.max(ratPos[axis] + (sign * SPEED), cheesePos[axis]);
     }
 
     rat.style[axis] = ratPos[axis] + "px";
+}
+
+
+/*
+    Makes ob1 go in the direction of obj2.
+
+    axis: left or top, the axis to move.
+*/
+function chase(obj1, obj2, axis) {
+    const speed = 0.8;
+
+    // Strategy to know if I should sum or subtract the speed.
+    let sign = (obj2.position[axis] - obj1.position[axis]);
+    sign = sign / Math.abs(sign);
+
+    // Round position
+    if (sign > 0) {
+        obj1.position[axis] = Math.min(
+            obj1.position[axis] + (sign * speed), 
+            obj2.position[axis]
+        );
+    }
+    else if (sign < 0) {
+        obj1.position[axis] = Math.max(
+            obj1.position[axis] + (sign * speed), 
+            obj2.position[axis]
+        );
+    }
+
+    obj1.style[axis] = obj1.position[axis] + "px";
 }
 
 function draw() {
@@ -82,6 +119,19 @@ function draw() {
         moveRat(rat, "top");
         shouldRender = true;
     }
+
+    // Update X coordinate
+    if (antPos.left !== ratPos.left) {
+        chase(ant, rat, "left");
+        shouldRender = true;
+    }
+    // Update Y coordinate
+    if (antPos.top !== ratPos.top) {
+        chase(ant, rat, "top");
+        shouldRender = true;
+    }
+
+    console.log(ant.position);
 
     if (!shouldRender) {
         cancelAnimationFrame(idAnimationFrame);
